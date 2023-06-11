619a3ac19afffbda410a5600741f8312de4492e4
Tej <tejpartapsingh91@gmail.com>
2023-06-11 14:19:35 -0400

    v0.0.1

--- forge.config.js
+++ forge.config.js
@@ -11,27 +11,4 @@ module.exports = {
       gatekeeperAssess: false,
     },
   },
-  makers: [
-    {
-      name: "@electron-forge/maker-squirrel",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-dmg",
-      platforms: ["darwin"],
-      config: {},
-    },
-    // {
-    //   name: "@electron-forge/maker-zip",
-    //   config: {},
-    // },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
-    },
-  ],
 };
